bd1c8953705a9815529ed00643303ee1a143411c
eyyubmermer <98224771+eyyubmermer@users.noreply.github.com>
2022-06-02 13:24:01 +0300

    start

---
+++ cmd/api/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"go-backend/models"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+const version string = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+}
+
+type AppStatus struct {
+	Status      string `json:"status"`
+	Environment string `json:"environment"`
+	Version     string `json:"version"`
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+	models models.Models
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4080, "the port that we deployed out app")
+	flag.StringVar(&cfg.env, "environment", "env", "Environment")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:123@localhost:5432/moviego?sslmode=disable", "Postgres connection string")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+		models: models.NewModels(db),
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
